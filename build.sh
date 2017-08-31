0b52297dca326aa8fd267e2cf7efdf9af6490553
Peter Bengtsson <mail@peterbe.com>
2017-08-31 11:10:25 -0400

    better build thing

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DESTINATION=~/Desktop/github-tricks-and-treats.zip
+DESTINATION=~/Desktop/github-tricks-and-treats-`cat manifest.json| jq .version | sed -e 's/^"//' -e 's/"$//'`.zip
 zip "$DESTINATION" `git ls-files`
 
 echo "Created $DESTINATION"
